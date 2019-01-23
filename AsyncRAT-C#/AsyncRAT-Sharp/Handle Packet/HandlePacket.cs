94a33646a8452965c4a94bc2b464646c27da058d
NYAN CAT <NYANxCAT@pm.me>
2019-01-23 01:03:27 -0800

    Update HandlePacket.cs

--- AsyncRAT-C#/AsyncRAT-Sharp/Handle Packet/HandlePacket.cs
+++ AsyncRAT-C#/AsyncRAT-Sharp/Handle Packet/HandlePacket.cs
@@ -7,6 +7,7 @@ namespace AsyncRAT_Sharp.Handle_Packet
 {
     class HandlePacket
     {
+        public delegate void UpdateListViewDelegatevoid(Clients Client, byte[] Data);
         public static void Read(Clients client, byte[] data)
         {
             MsgPack unpack_msgpack = new MsgPack();
@@ -14,15 +15,19 @@ namespace AsyncRAT_Sharp.Handle_Packet
             switch (unpack_msgpack.ForcePathObject("Packet").AsString)
             {
                 case "ClientInfo":
-                        Program.form1.Invoke((MethodInvoker)delegate ()
-                        {
-                            client.LV = new ListViewItem();
-                            client.LV.Tag = client;
-                            client.LV.Text = string.Concat(client.client.RemoteEndPoint.ToString());
-                            client.LV.SubItems.Add(unpack_msgpack.ForcePathObject("User").AsString);
-                            client.LV.SubItems.Add(unpack_msgpack.ForcePathObject("OS").AsString);
-                            Program.form1.listView1.Items.Insert(0, client.LV);
-                        });
+                    if (Program.form1.InvokeRequired)
+                    {
+                        Program.form1.Invoke(new UpdateListViewDelegatevoid(Read), new object[] { client, data });
+                    }
+                    else
+                    {
+                        client.LV = new ListViewItem();
+                        client.LV.Tag = client;
+                        client.LV.Text = string.Concat(client.client.RemoteEndPoint.ToString());
+                        client.LV.SubItems.Add(unpack_msgpack.ForcePathObject("User").AsString);
+                        client.LV.SubItems.Add(unpack_msgpack.ForcePathObject("OS").AsString);
+                        Program.form1.listView1.Items.Insert(0, client.LV);
+                    }
                     break;                             
             }
         }
