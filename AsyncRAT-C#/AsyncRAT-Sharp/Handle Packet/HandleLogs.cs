9f5dfc075434ed6a1c10cdfa68766ae5920a464a
NYAN CAT <NYANxCAT@pm.me>
2019-05-24 23:44:13 +0300

    Update HandleLogs.cs

--- AsyncRAT-C#/AsyncRAT-Sharp/Handle Packet/HandleLogs.cs
+++ AsyncRAT-C#/AsyncRAT-Sharp/Handle Packet/HandleLogs.cs
@@ -12,19 +12,29 @@ namespace AsyncRAT_Sharp.Handle_Packet
     {
         public void Addmsg(string Msg, Color color)
         {
-            if (Program.form1.listView2.InvokeRequired)
+            try
             {
-                Program.form1.listView2.BeginInvoke((MethodInvoker)(() =>
+                if (Program.form1.listView2.InvokeRequired)
+                {
+                    Program.form1.listView2.BeginInvoke((MethodInvoker)(() =>
+                    {
+                        ListViewItem LV = new ListViewItem();
+                        LV.Text = DateTime.Now.ToLongTimeString();
+                        LV.SubItems.Add(Msg);
+                        LV.ForeColor = color;
+                        Program.form1.listView2.Items.Insert(0, LV);
+                    }));
+                }
+                else
                 {
                     ListViewItem LV = new ListViewItem();
                     LV.Text = DateTime.Now.ToLongTimeString();
                     LV.SubItems.Add(Msg);
                     LV.ForeColor = color;
-                    Program.form1.listView2.BeginUpdate();
                     Program.form1.listView2.Items.Insert(0, LV);
-                    Program.form1.listView2.EndUpdate();
-                }));
+                }
             }
+            catch { }
         }
     }
 }
