a1b2ddb0205cff8f7e74cf38df81031ae5827641
NYAN CAT <NYANxCAT@pm.me>
2019-05-29 21:20:55 +0300

    Add notification

--- AsyncRAT-C#/AsyncRAT-Sharp/Handle Packet/HandleReportWindow.cs
+++ AsyncRAT-C#/AsyncRAT-Sharp/Handle Packet/HandleReportWindow.cs
@@ -5,6 +5,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace AsyncRAT_Sharp.Handle_Packet
 {
@@ -13,6 +14,17 @@ namespace AsyncRAT_Sharp.Handle_Packet
         public HandleReportWindow(Clients client, string title)
         {
             new HandleLogs().Addmsg($"Client {client.ClientSocket.RemoteEndPoint.ToString().Split(':')[0]} Opened [{title}]", Color.Blue);
+            if (Program.form1.InvokeRequired)
+            {
+                Program.form1.BeginInvoke((MethodInvoker)(() =>
+                {
+                    if (Properties.Settings.Default.Notification == true)
+                    {
+                        Program.form1.notifyIcon1.BalloonTipText = $"Client {client.ClientSocket.RemoteEndPoint.ToString().Split(':')[0]} Opened [{title}]";
+                        Program.form1.notifyIcon1.ShowBalloonTip(100);
+                    }
+                }));
+            }
         }
     }
 }
