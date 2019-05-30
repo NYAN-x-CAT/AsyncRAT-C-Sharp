e3ee206e8beff0fabde544de2b131a96e1481ce9
NYAN CAT <NYANxCAT@pm.me>
2019-05-30 23:21:25 +0300

    Update FormRemoteDesktop.Designer.cs

--- AsyncRAT-C#/AsyncRAT-Sharp/Forms/FormRemoteDesktop.Designer.cs
+++ AsyncRAT-C#/AsyncRAT-Sharp/Forms/FormRemoteDesktop.Designer.cs
@@ -33,6 +33,7 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.panel1 = new System.Windows.Forms.Panel();
+            this.btnMouse = new System.Windows.Forms.Button();
             this.btnSave = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
@@ -41,7 +42,6 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.timerSave = new System.Windows.Forms.Timer(this.components);
-            this.btnMouse = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
@@ -53,7 +53,7 @@
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1012, 674);
+            this.pictureBox1.Size = new System.Drawing.Size(938, 485);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -79,8 +79,19 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1012, 38);
+            this.panel1.Size = new System.Drawing.Size(938, 38);
             this.panel1.TabIndex = 1;
+            // 
+            // btnMouse
+            // 
+            this.btnMouse.BackgroundImage = global::AsyncRAT_Sharp.Properties.Resources.mouse;
+            this.btnMouse.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnMouse.Location = new System.Drawing.Point(550, 3);
+            this.btnMouse.Name = "btnMouse";
+            this.btnMouse.Size = new System.Drawing.Size(32, 32);
+            this.btnMouse.TabIndex = 3;
+            this.btnMouse.UseVisualStyleBackColor = true;
+            this.btnMouse.Click += new System.EventHandler(this.Button3_Click);
             // 
             // btnSave
             // 
@@ -174,7 +185,7 @@
             this.button2.BackgroundImage = global::AsyncRAT_Sharp.Properties.Resources.arrow_up;
             this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Location = new System.Drawing.Point(304, 45);
+            this.button2.Location = new System.Drawing.Point(428, 44);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(18, 18);
             this.button2.TabIndex = 2;
@@ -187,22 +198,11 @@
             this.timerSave.Interval = 1500;
             this.timerSave.Tick += new System.EventHandler(this.TimerSave_Tick);
             // 
-            // btnMouse
-            // 
-            this.btnMouse.BackgroundImage = global::AsyncRAT_Sharp.Properties.Resources.mouse;
-            this.btnMouse.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btnMouse.Location = new System.Drawing.Point(550, 3);
-            this.btnMouse.Name = "btnMouse";
-            this.btnMouse.Size = new System.Drawing.Size(32, 32);
-            this.btnMouse.TabIndex = 3;
-            this.btnMouse.UseVisualStyleBackColor = true;
-            this.btnMouse.Click += new System.EventHandler(this.Button3_Click);
-            // 
             // FormRemoteDesktop
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1012, 674);
+            this.ClientSize = new System.Drawing.Size(938, 485);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.pictureBox1);
