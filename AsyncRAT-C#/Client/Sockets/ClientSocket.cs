79a7b5d50a254fd8be1b4ccf8e480438e8ef676c
NYAN CAT <NYANxCAT@pm.me>
2019-06-17 12:12:35 +0300

    Update ClientSocket.cs

--- AsyncRAT-C#/Client/Sockets/ClientSocket.cs
+++ AsyncRAT-C#/Client/Sockets/ClientSocket.cs
@@ -13,6 +13,7 @@ using System.Net.Security;
 using System.Security.Authentication;
 using System.Security.Cryptography.X509Certificates;
 using System.Net;
+using System.Text.RegularExpressions;
 
 //       │ Author     : NYAN CAT
 //       │ Name       : Nyan Socket v0.1
