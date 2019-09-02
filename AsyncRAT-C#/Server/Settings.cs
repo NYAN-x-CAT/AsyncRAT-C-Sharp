52f6944dfcbe8d1e13508bd78b2b70dddcaaf436
NYAN CAT <NYANxCAT@pm.me>
2019-09-02 08:02:42 +0300

    Update Settings.cs

--- AsyncRAT-C#/Server/Settings.cs
+++ AsyncRAT-C#/Server/Settings.cs
@@ -9,7 +9,6 @@ namespace Server
 {
     public static class Settings
     {
-        public static List<Clients> Online = new List<Clients>();
         public static List<string> Blocked = new List<string>();
         public static long Sent { get; set; }
         public static long Received { get; set; }
@@ -17,8 +16,8 @@ namespace Server
         public static string CertificatePath = Application.StartupPath + "\\ServerCertificate.p12";
         public static X509Certificate2 ServerCertificate;
         public static readonly string Version = "AsyncRAT 0.5.2";
-        public static object Listview1Lock = new object();
-        public static object Listview2Lock = new object();
-        public static object Listview3Lock = new object();
+        public static object LockListviewClients = new object();
+        public static object LockListviewLogs = new object();
+        public static object LockListviewThumb = new object();
     }
 }
