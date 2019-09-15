bb56311d71de489074ec04a6c80afe1a1701c5bf
NYAN CAT <NYANxCAT@pm.me>
2019-09-15 16:01:05 +0300

    Update Settings.cs

--- AsyncRAT-C#/Client/Settings.cs
+++ AsyncRAT-C#/Client/Settings.cs
@@ -12,7 +12,7 @@ namespace Client
 #if DEBUG
         public static string Ports = "6606";
         public static string Hosts = "127.0.0.1";
-        public static string Version = "0.5.2";
+        public static string Version = "0.5.3";
         public static string Install = "false";
         public static string InstallFolder = "AppData";
         public static string InstallFile = "Test.exe";
