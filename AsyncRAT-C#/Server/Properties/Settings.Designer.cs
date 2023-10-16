68c7666dc256dd1297df6863b1f2450fa7c38f11
NYAN CAT <32758426+NYAN-x-CAT@users.noreply.github.com>
2023-10-16 17:20:22 +0300

    Update Settings.Designer.cs

--- AsyncRAT-C#/Server/Properties/Settings.Designer.cs
+++ AsyncRAT-C#/Server/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Server.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.5.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -61,7 +61,7 @@ namespace Server.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("AsyncMutex_6SI8OkPnk")]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
         public string Mutex {
             get {
                 return ((string)(this["Mutex"]));
