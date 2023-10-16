b8101babadb2cb19aa09a41bb235a205e1ce57a7
NYAN CAT <32758426+NYAN-x-CAT@users.noreply.github.com>
2023-10-16 17:20:41 +0300

    Create NativeMethods.cs

---
+++ AsyncRAT-C#/Server/StreamLibrary/NativeMethods.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.StreamLibrary
+{
+    public static class NativeMethods
+    {
+        [DllImport("msvcrt.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern unsafe int memcmp(byte* ptr1, byte* ptr2, uint count);
+
+        [DllImport("msvcrt.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int memcpy(IntPtr dst, IntPtr src, uint count);
+
+        [DllImport("msvcrt.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern unsafe int memcpy(void* dst, void* src, uint count);
+    }
+}
